1390b8c09e233884863614860036e7bcfbf80e05
inwebo <github@inwebo.net>
2020-10-04 15:10:18 +0200

    Update

--- docs/assets/js/demo.js
+++ docs/assets/js/demo.js
@@ -1,7 +1,11 @@
 import Renderer2D from "@inwebo/render.js/src/Renderer2D";
+import Node from "../../../src/Graph/Node.js";
+import {Vector2D} from "@inwebo/vector";
+import Graph from "../../../src/Graph/Graph";
 
 window.addEventListener("DOMContentLoaded", (event) => {
     const canvas = document.getElementById('demo');
-    const render = new Renderer2D(canvas);
+    // const render = new Renderer2D(canvas);
+
 
 });
