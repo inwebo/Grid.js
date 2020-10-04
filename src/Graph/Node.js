281ba463dc8cbe0fdccfe0459248eb1a2dcce834
inwebo <github@inwebo.net>
2020-10-04 19:22:42 +0200

    Typo

--- src/Graph/Node.js
+++ src/Graph/Node.js
@@ -15,7 +15,7 @@ export default class Node {
      * @param {*} key
      * @param {Node} node
      */
-    addNeighbors(key, node) {
+    addNeighbor(key, node) {
         this._neighbors.set(key, node);
     }
 
@@ -26,8 +26,30 @@ export default class Node {
         return this._neighbors;
     }
 
+    /**
+     * @param {*} key
+     * @return {Node|boolean}
+     */
     getNeighbor(key) {
-        return this._neighbors.get(key);
+        if(this.hasNeighbor(key)) {
+            return this._neighbors.get(key);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param {*} key
+     * @param {*} revertBy
+     * @return {boolean}
+     */
+    isBiDirectional(key, revertBy) {
+        if(this.hasNeighbor(key)) {
+            const neighbor = this.getNeighbor(key);
+            return neighbor.hasNeighbor(revertBy);
+        }
+
+        return false;
     }
 
     /**
