79ab2e2f668f0a699a6c91efb51a9d0ea6578cf0
inwebo <github@inwebo.net>
2020-10-04 15:08:08 +0200

    Add graph & node

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
+// Cells
 import Cell from "./Cells/Cell";
 import CellAbstract from "./Cells/CellAbstract";
 
+// Converters
 import ToPixels from "./Converters/ToPixels";
 
+// Coordinates
 import Cartesian from "./Coordinates/Cartesian";
 import CoordinatesAbstract from "./Coordinates/CoordinatesAbstract";
 import Isometric from "./Coordinates/Isometric";
 
+// Graph
+import Graph from "./Graph/Graph";
+import Node from "./Graph/Node";
+
+// Grid
 import Grid from "./Grid/Grid";
 
 export {
     Cell,
     CellAbstract,
+
+    ToPixels,
+
     Cartesian,
     CoordinatesAbstract,
     Isometric,
+
+    Graph,
+    Node,
+
     Grid,
-    ToPixels,
 };
