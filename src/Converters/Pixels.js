788cdb622eb675bf50e19b6240ae0bf87fc86a7d
inwebo <github@inwebo.net>
2020-10-08 19:52:40 +0200

    Add doc

--- src/Converters/Pixels.js
+++ src/Converters/Pixels.js
@@ -1,8 +1,24 @@
+import {Vector2D} from "@inwebo/vector";
+
 /**
- * Convert a cell matrix position to a canvas width and height pixel position
+ * Convert a cell matrix position to a canvas width and height pixel position for an image.
  */
 export default class Pixels {
-    convert() {
+
+    /**
+     * @param {Vector2D} position
+     * @param {Vector2D} canvasSize
+     */
+    constructor(position, canvasSize) {
+        this._position   = position;
+        this._canvasSize = canvasSize;
+    }
+
+    /**
+     * @param {Vector2D} imageSize
+     * @param {Vector2D} offset
+     */
+    convert(imageSize, offset) {
 
     }
 }
