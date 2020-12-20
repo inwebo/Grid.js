41cf52abc1ac59f94298d523fb0c21ef1ec040ea
inwebo <github@inwebo.net>
2020-12-20 11:09:57 +0100

    Introduce tests script

---
+++ test/tests.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const chai = require('chai');
+
+describe('test', () => {
+    describe('test', () => {
+        assert.equal(true, true);
+    });
+});
