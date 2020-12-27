ad0c3921643a064b1c37403a739876c366bf6a1e
inwebo <github@inwebo.net>
2020-12-27 22:28:33 +0100

    Populate

--- docs/assets/js/demos/demo-1.js
+++ docs/assets/js/demos/demo-1.js
@@ -52,15 +52,87 @@ const demo1 = () => {
 
     renderGrid._draw([grid, tileSize, cellOffset, form.colours.first.value, form.colours.second.value]);
 
-    const ac = grid.assertCell(0,9, (cell) => {
-        return cell !== undefined;
-    })
+    class C extends Cell {
+        setData(data) {
+            this._data = data;
+        }
 
-    grid.fill(() => {
-        return false;
+        getData() {
+            return this._data;
+        }
+
+        constructor(vector) {
+            super(vector);
+            this._data = null;
+        }
+    }
+
+    const g = new Grid(new Vector2D(5,5), null, ([x, y]) => {
+        return new C(new Vector2D(x,y));
     });
 
-    console.log(grid.getRows());
+    /**
+     * @param {Grid} grid
+     */
+    const populateGrid = (grid) => {
+
+        while (grid.assertCells((cell) => {
+
+        }) !== false);
+    };
+
+    const start = g.assertCells((cell) => {
+        return cell.getData() === null;
+    });
+
+    const rows = g.getRow(start.getIndex().getY(), start.getIndex().getY());
+    const cols = g.getCol(start.getIndex().getX(), start.getIndex().getX());
+
+    const char = Array.from('ABCDEFGHIJKLMN');
+
+
+    let freeX = 0;
+    let freeY = 0;
+
+    for(const cell of rows) {
+        if(cell.getData() !== null) {
+            break;
+        }
+        freeX += 1;
+    }
+
+    for(const cell of cols) {
+        if(cell.getData() !== null) {
+            break;
+        }
+        freeY += 1;
+    }
+
+    console.log(freeX, freeY);
+
+    /**
+     * @param {int} a Min int
+     * @param {int} b Max int
+     * @return {number}
+     */
+    const rand = (a,b) => a+(b-a+1)*crypto.getRandomValues(new Uint32Array(1))[0]/2**32|0;
+
+    const MAX_SIZE = 3;
+
+    const _currentMax = (freeX >= MAX_SIZE) ? MAX_SIZE : freeX;
+
+    const maxRand = rand(1, _currentMax);
+    console.log(maxRand)
+    const c = char.shift();
+    for(let x = 0; x < freeX; x++) {
+        for(let y = 0; y < freeY; y++) {
+            if(x <= maxRand && y <= maxRand) {
+                g.getCell(x, y).setData(c);
+            }
+        }
+    }
+
+    console.log(g.getRows());
 
 };
 
