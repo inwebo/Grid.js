7dacfd6026ad8599616ed52c40749ab0bbd51f0d
inwebo <github@inwebo.net>
2020-12-27 23:37:19 +0100

    end populate

--- docs/assets/js/demos/demo-1.js
+++ docs/assets/js/demos/demo-1.js
@@ -67,48 +67,15 @@ const demo1 = () => {
         }
     }
 
+    /**
+     * Subject
+     * @type {Grid}
+     */
     const g = new Grid(new Vector2D(5,5), null, ([x, y]) => {
         return new C(new Vector2D(x,y));
     });
 
-    /**
-     * @param {Grid} grid
-     */
-    const populateGrid = (grid) => {
-
-        while (grid.assertCells((cell) => {
-
-        }) !== false);
-    };
-
-    const start = g.assertCells((cell) => {
-        return cell.getData() === null;
-    });
-
-    const rows = g.getRow(start.getIndex().getY(), start.getIndex().getY());
-    const cols = g.getCol(start.getIndex().getX(), start.getIndex().getX());
-
     const char = Array.from('ABCDEFGHIJKLMN');
-
-
-    let freeX = 0;
-    let freeY = 0;
-
-    for(const cell of rows) {
-        if(cell.getData() !== null) {
-            break;
-        }
-        freeX += 1;
-    }
-
-    for(const cell of cols) {
-        if(cell.getData() !== null) {
-            break;
-        }
-        freeY += 1;
-    }
-
-    console.log(freeX, freeY);
 
     /**
      * @param {int} a Min int
@@ -119,21 +86,94 @@ const demo1 = () => {
 
     const MAX_SIZE = 3;
 
-    const _currentMax = (freeX > MAX_SIZE) ? MAX_SIZE : freeX;
+    /**
+     * @param {Grid} grid
+     * @param {C} start
+     */
+    const populateGrid = (grid, start) => {
+        console.log('Start cell',start);
+        const rows = g.getRow(start.getIndex().getY(), start.getIndex().getY());
+        const cols = g.getCol(start.getIndex().getX(), start.getIndex().getX());
 
-    const maxRand = rand(1, _currentMax);
-    console.log(maxRand)
-    const c = char.shift();
-    for(let x = 0; x < maxRand; x++) {
-        for(let y = 0; y < maxRand; y++) {
-            if(x <= maxRand && y <= maxRand) {
-                g.getCell(x, y).setData(c);
+        let freeX = 0;
+        let freeY = 0;
+
+        for(const cell of rows) {
+            if(cell.getData() !== null) {
+                break;
+            }
+            freeX += 1;
+        }
+
+        for(const cell of cols) {
+            if(cell.getData() !== null) {
+                break;
+            }
+            freeY += 1;
+        }
+
+        console.log('FreeX', freeX, 'FreeY', freeY);
+
+        let _currentMax = ((freeX > freeY) ? freeX : freeY);
+        _currentMax = (_currentMax > MAX_SIZE) ? MAX_SIZE : _currentMax;
+
+        const maxRand = rand(1, _currentMax);
+        console.log('Max rand', maxRand)
+        const c = char.shift();
+        for(let x = 0; x < maxRand; x++) {
+            for(let y = 0; y < maxRand; y++) {
+                if(x <= maxRand && y <= maxRand) {
+                    g.getCell(start.getIndex().getX() + x, start.getIndex().getY() + y).setData(c);
+                }
             }
         }
-    }
+
+        const s = g.assertCells((cell) => {
+            return cell.getData() === null;
+        });
+
+        console.log('Next cell', s);
+
+        // if(s.getData() === null) {
+        //     populateGrid(g, s);
+        // }
+    };
+
+    const start = g.assertCells((cell) => {
+        return cell.getData() === null;
+    });
+
+    populateGrid(g, start);
+
+    // var s = g.assertCells((cell) => {
+    //     return cell.getData() === null;
+    // });
+    //
+    // if(s !== false) {
+    //     populateGrid(g, s);
+    // }
+    //
+    // var s = g.assertCells((cell) => {
+    //     return cell.getData() === null;
+    // });
+    //
+    // if(s !== false) {
+    //     populateGrid(g, s);
+    // }
 
     console.log(g.getRows());
 
+    while ( g.assertCells((cell) => {
+        return cell.getData() === null;
+    }) !== false) {
+        const start = g.assertCells((cell) => {
+            return cell.getData() === null;
+        });
+
+        populateGrid(g, start);
+    }
+
+    console.log(g.getRows());
 };
 
 export default demo1;
