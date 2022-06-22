b3faa2ccedc65b34234fcbe8e47bed86e55e51bb
Inwebo <github@inwebo.net>
2022-06-22 15:06:52 +0200

    Update

--- _docs/index.js
+++ _docs/index.js
@@ -1,5 +1,34 @@
 import demo1 from "./js/demo-1";
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    demo1()
+    demo1();
+
+    document.getElementById('tile-width').onchange = () => {
+        demo1()
+    }
+
+    document.getElementById('tile-height').onchange = () => {
+        demo1()
+    }
+
+    document.getElementById('cols').onchange = () => {
+        demo1()
+    }
+    document.getElementById('rows').onchange = () => {
+        demo1()
+    }
+
+    document.getElementById('cell-offset-x').onchange = () => {
+        demo1()
+    }
+    document.getElementById('cell-offset-y').onchange = () => {
+        demo1()
+    }
+
+    document.getElementById('colour1').onchange = () => {
+        demo1()
+    }
+    document.getElementById('colour2').onchange = () => {
+        demo1()
+    }
 });
